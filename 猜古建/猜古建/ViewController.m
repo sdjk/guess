7f0fb8d2a1f23b229addbdde69b9b4289d6d73b2
XFP <sdjkxas@163.com>
2016-08-31 04:46:27 +0800

    添加了Button，创建了plist文件

--- 猜古建/猜古建/ViewController.m
+++ 猜古建/猜古建/ViewController.m
@@ -61,6 +61,10 @@
     
     NSLog(@"%@",self.myButton);
     
+    [self createAnswerButton];
+    
+    [self createOptionButton];
+   
 }
 
 
@@ -107,11 +111,78 @@
         }];
     }
     
-    
-    
 }
 
+// 创建答案区按钮,测试
+-(void) createAnswerButton
+{
+    for (int i = 0 ; i < 3; i++)
+    {
+        // 创建UIButton
+        UIButton *but = [UIButton buttonWithType:UIButtonTypeCustom];
+        
+        //  设置背景图
+        [but setBackgroundImage:[UIImage imageNamed:@"btn_answer"] forState:UIControlStateNormal];
+        
+        [but setBackgroundImage:[UIImage imageNamed:@"btn_answer_highlighted"] forState:UIControlStateHighlighted];
+        
+        // 设置每个Button的frame
+        
+        
+        // 宽度要等于高度
+        CGFloat width = self.answerView.frame.size.height;
+        
+        // 设置每个button的边距
+        CGFloat margin = (self.answerView.frame.size.width - 150 - 3 * width) / (3 - 1);
+        
+        // 要设置X值，设置两边的间距分别为50
+        CGFloat x = 75 + i * (margin + width);
+        
+        but.frame = CGRectMake(x, 0, width, width);
+        
+        [self.answerView addSubview:but];
+        
+    }
+}
 
+// 创建被选区
+- (void) createOptionButton
+{
+    // 也使用for循环，进行九宫格布局，3行7列，一起21个选项
+    for (int i = 0; i < 21; i ++)
+    {
+        //  用模取列，可以求x值
+        int column = i % 7;
+        
+        // 用除取行，可以求y值
+        int row = i / 7;
+        
+        //  设置间距
+        CGFloat margin = 10;
+        
+        UIButton *but = [UIButton buttonWithType:UIButtonTypeCustom];
+        
+        [but setBackgroundImage:[UIImage imageNamed:@"btn_option"] forState:UIControlStateNormal];
+        
+        [but setBackgroundImage:[UIImage imageNamed:@"btn_option_highlighted"] forState:UIControlStateHighlighted];
+        
+        CGFloat width = (self.optionView.frame.size.width - 8 * margin) / 7;
+        
+        CGFloat yMargin = (self.optionView.frame.size.height - 3 * width - 2 * margin) * 0.5;
+        
+        
+        CGFloat x = margin + column * (margin + width);
+        
+        CGFloat y = yMargin + row * (margin + width);
+        
+        but.frame = CGRectMake(x, y, width, width);
+        
+        [self.optionView addSubview:but];
+        
+        
+    }
+    
+}
 
 
 - (void)didReceiveMemoryWarning {
